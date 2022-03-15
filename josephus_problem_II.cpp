47d52bdda13612b85192892c229d337f8c9dd31d
Mohammad Shahanwaz <mohdshahanwaz55@gmail.com>
2022-03-15 19:43:39 +0530

    Add

---
+++ josephus_problem_II.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+using namespace std;
+using ll = long long;
+
+#define endl '\n'
+#define fastIO cin.tie(0)->sync_with_stdio(0)
+template<class T> using ordered_set =tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update>;
+
+int main() {
+    fastIO;
+    int TC = 1;
+    // cin >> TC;
+    while (TC--) {
+        int n, k;
+        cin >> n >> k;
+        int p = k % n;
+        ordered_set<int> a;
+        for (int i = 1; i <= n; ++i) {
+            a.insert(i);
+        }
+        while (a.size()) {
+            int r = *a.find_by_order(p);
+            a.erase(r);
+            if (a.size())
+                p = (p + k) % a.size();
+            cout << r << " ";
+        }
+    }
+    return 0;
+}
